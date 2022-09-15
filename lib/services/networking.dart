0ba887ce0e86308db63782a8d611e2e4eefacbed
Alessandro <73858113+TT-zero@users.noreply.github.com>
2022-09-16 10:58:29 +1200

    add pluggable vars to apiCall

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,114 +1,65 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-
-
-// api call for BTC and ETH coin Rate:::::::
-Future<CoinData> getCoinRates(String coin) async {
-  
-  String cryptoCoin = '';
-  coin == 'BTC' ? cryptoCoin = 'BTC': cryptoCoin = 'ETH';
-  var apiUrl = 'https://rest.coinapi.io/v1/exchangerate/$cryptoCoin/NZD?apikey=5FD2E560-74A0-413D-A23D-F908EA582C1C';
+Future<CoinData> getCoinRates(String crypto, String fiat) async {
+  var apiUrl =
+      'https://rest.coinapi.io/v1/exchangerate/$crypto/$fiat?apikey=5FD2E560-74A0-413D-A23D-F908EA582C1C';
 
   var response = await http.get(Uri.parse(apiUrl));
 
-  if (response.statusCode == 200){
+  if (response.statusCode == 200) {
     CoinData coinRate = CoinData.fromJson(jsonDecode(response.body));
-    print (coinRate.rate);
+    print(coinRate.rate);
     return coinRate;
-  }
-
-  else {
-    print (response.statusCode);
+  } else {
+    print(response.statusCode);
     throw Exception('error : ${response.statusCode}');
   }
 }
 
-// API CALL FOR ALTS, RETURN ALL COIN RATES FROM ONE FUTURE CALL
 Future<List<double>> getAltRates() async {
   String coinApiKey = '5FD2E560-74A0-413D-A23D-F908EA582C1C';
   List<String> cryptosList = ['LUNA', 'DOT', 'SOL', 'FTM', 'MATIC'];
   List<String> cryptoUrls = [];
 
   for (String crypto in cryptosList) {
-    var newUrl = 'https://rest.coinapi.io/v1/exchangerate/$crypto/NZD?apikey=$coinApiKey';
+    var newUrl =
+        'https://rest.coinapi.io/v1/exchangerate/$crypto/NZD?apikey=$coinApiKey';
     cryptoUrls.add(newUrl);
   }
-  
-  List<double> coinRates = [];
 
+  List<double> coinRates = [];
 
   for (String cryptoUrl in cryptoUrls) {
     var response = await http.get(Uri.parse(cryptoUrl));
-    
-    if (response.statusCode == 200){
-    CoinData coinData = CoinData.fromJson(jsonDecode(response.body));
-    print (coinData.rate);
-    double rateNow = coinData.rate.toDouble();
-    coinRates.add(rateNow);
-    
+
+    if (response.statusCode == 200) {
+      CoinData coinData = CoinData.fromJson(jsonDecode(response.body));
+      print(coinData.rate);
+      double rateNow = coinData.rate.toDouble();
+      coinRates.add(rateNow);
     } else {
       print(response.statusCode);
-      throw Exception('there is an error in your request. HTTP: ${response.statusCode}');
+      throw Exception(
+          'there is an error in your request. HTTP: ${response.statusCode}');
     }
   }
-  print (coinRates);
+  print(coinRates);
   return coinRates;
 }
-
-
-
-
-
-
-
 
 class CoinData {
   double rate;
 
-  CoinData({
-    required this.rate
-  });
+  CoinData({required this.rate});
 
-  factory CoinData.fromJson(Map<String, dynamic> json){
+  factory CoinData.fromJson(Map<String, dynamic> json) {
     return CoinData(
       rate: json['rate'],
     );
   }
 
   Map<String, dynamic> toJson() => {
-    'rate' : rate,
-  };
+        'rate': rate,
+      };
 }
-
-
-
-
-
-
-
-
-// original that works
-
-// Future<CoinData> getCoinRates() async {
-//   String apiUrl = 'https://rest.coinapi.io/v1/exchangerate/LUNA/NZD?apikey=5FD2E560-74A0-413D-A23D-F908EA582C1C';
-//   var response = await http.get(Uri.parse(apiUrl));
-
-//   if (response.statusCode == 200){
-//     CoinData coinData = CoinData.fromJson(jsonDecode(response.body));
-//     // OR
-//     // Map<String, dynamic> coinDataMap = jsonDecode(response.body);
-//     // var coinData = CoinData.fromJson(coinDataMap);
-    
-//     // coinRates.add(coinData.rate);
-
-//     print(coinData.rate);
-//     print('hello ${coinData.rate}!');
-//     return coinData;
-
-//   } else {
-//     print(response.statusCode);
-//     throw Exception('error in your request');
-//   }
-// }
-
